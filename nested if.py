dfd88496c7dab78e48ada44a892af96524650e1f
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-04-17 22:47:18 +0530

    python_program

---
+++ nested if.py
@@ -0,0 +1,14 @@
+a=int(input("Enter A : "))
+b=int(input("Enter B : "))
+c=int(input("Enter C : "))
+
+if a>b :
+    if a>c :
+        print("A is Max")
+    else:
+        print("C is Max")
+elif b>c:
+    print("B is max")
+else:
+    print("C is Max")
+        
