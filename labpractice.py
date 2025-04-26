66971aacbd1d87510fbea7d13d2301d0808030ec
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-04-27 00:00:58 +0530

    python_Program

---
+++ labpractice.py
@@ -0,0 +1,15 @@
+n=int(input("Enter N : "))
+sum=0
+for i in range(1,n+1):
+        sum=sum+i
+print("Sum : ",sum)
+
+
+n=int(input("Enter N : "))
+sum=0
+while n>0:
+    sum=sum+n
+    n=n-1
+        
+print("Sum : ",sum)
+
