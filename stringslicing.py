3baf32c41e9aa5896413677073acde767efcfb71
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-04-28 16:03:55 +0530

    python_Program

---
+++ stringslicing.py
@@ -0,0 +1,20 @@
+#  01234567890123456      +
+s="Tops Technologies"
+#  76543210987654321      -
+
+print(s[3:13])
+print(s[:15])
+print(s[2:])
+print(s[1:16:3])
+print(s[::5])
+
+
+
+print(s[-15:-3])
+print(s[:-5])
+print(s[-16:])
+print(s[-15:-3:2])
+print(s[::-1])
+
+
+
