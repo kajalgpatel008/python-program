f8326653348821cfca56b546557e11893785aad4
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-04-28 16:01:31 +0530

    python_Program

---
+++ stringfunction.py
@@ -0,0 +1,45 @@
+s="Tops Technologies"
+
+print(s)
+print(s.capitalize())
+print(s.casefold())
+print(s.center(40,"*"))
+print(s.count("o"))
+print(s.endswith("es"))
+print(s.find("log"))
+print(s.index("T",2))
+print("tops123".isalnum())
+print("tops".isalnum())
+print("123".isalnum())
+print("tops".isalpha())
+print("123".isnumeric())
+print(" ".isspace())
+print(s.istitle())
+print("tops".islower())
+print("TOPS".isupper())
+print(s.lower())
+print(s.upper())
+print("Hello".replace("l","w"))
+print(s.startswith("To"))
+print(s.swapcase())
+print("python programming".title())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
