f3b8a8ee4aca426d76a5d164518152e58dfe2838
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-04-28 16:03:04 +0530

    python_Program

---
+++ practiceslic..py
@@ -0,0 +1,24 @@
+#  01234567890123456     +           
+s="Tops Technologies"
+#  76543210987654321     -
+
+print(s[4:])
+print(s[:7])
+print(s[4:16])                ##end range not count means 16 then to 15
+print(s[::])
+print(s[2:13:2])
+print(s[::4])
+print(s[1:5])
+
+
+print(s[-4:])
+print(s[:-7])
+print(s[-4:-16])    ##            ##end range not count means 16 then to 15
+print(s[::])
+print(s[-2:-13:-2])   #
+print(s[::-4])
+print(s[-1:-5])       ##
+
+
+
+
