b8c29918ed58bc5da85a9b15d3fd87d5c7cf72f2
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-05-05 14:46:18 +0530

    python

---
+++ lab9.slicing.py
@@ -0,0 +1,19 @@
+
+s="Kajal M Patel"
+
+print(s[3:13])
+print(s[:15])
+print(s[2:])
+print(s[1:16:3])
+print(s[::5])
+
+
+
+print(s[-15:-3])
+print(s[:-5])
+print(s[-16:])
+print(s[-15:-3:2])
+print(s[::-1])
+
+
+
