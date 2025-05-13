9a6064833f758965e87d3f92c2be587314ccf11f
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-05-13 21:27:25 +0530

    python example

---
+++ class demo.py
@@ -0,0 +1,16 @@
+class Student:
+
+    def getData(self,fname,lname):
+        self.f=fname
+        self.l=lname
+    def putData(self):
+        print("First Name : ",self.f)
+        print("last Name : ",self.l)
+
+
+s1=Student()
+
+s1.getData("Jigar","Thakkar")
+s1.putData()
+
+
