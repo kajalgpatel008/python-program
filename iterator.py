0ba72c094d6227260c774f441416ff6c02687c48
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-06-27 23:35:14 +0530

    python program

---
+++ iterator.py
@@ -0,0 +1,24 @@
+# Creating an iterator from a list
+
+numbers=[1,2,3]
+iterator=iter(numbers)
+
+print(next(iterator))  #output: 1
+print(next(iterator))  #output: 2
+print(next(iterator))  #output: 3
+print(next(iterator))  # Raises StopIteration 
+print(next(iterator))
+
+
+
+import math
+print(math.ceil(12.34))
+
+
+print(math.pi)
+print(math.e)
+print(math.sin(0.52))
+print(math.cos(0.52))
+print(math.tan(0.52))
+print(math.log(0.52))
+
