5502f52d127f08c364f7be587dbe25106c64355a
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-08-06 17:26:47 +0530

    python program

---
+++ primefunction.py
@@ -0,0 +1,16 @@
+def prime(n):
+   if n%2!=0:
+            for i in range(3,int(n/2)+1,2):
+                if n%i==0:
+                     print(n,"Is Not Prime")
+                     break
+            else:
+                    print(n,"Is Prime")
+   else: 
+          print(n, "Is Not Prime")
+
+n=int(input("Enter N : "))
+prime(n)
+
+        
+   
