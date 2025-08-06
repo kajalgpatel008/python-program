49fac6dac0b3273a9ecd83aa75ff610ef2051f24
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-08-06 17:23:37 +0530

    python program

---
+++ practice,.py
@@ -0,0 +1,46 @@
+s="Tops Technologies"
+
+print(s)
+
+print(s.capitalize())
+print(s.casefold())
+print(s.center(40,"*"))
+print(s.count("s"))
+print(s.endswith("p"))    ##bool
+print(s.find("ch"))
+print(s.index("o",2))
+print("567".isalnum())    ##bool
+print("km".isalpha())
+print("18152659864516235464131".isnumeric())    ##numeric-0-9 any no -
+print("tops".islower())
+print(" ".isspace())
+print("kops".istitle())     #first alpha-title
+print("Tops".isupper())
+print(s.lower())
+print(s.upper())
+print("Hello".replace("e","p"))
+print("kajal".swapcase())
+print(s.startswith("P"))
+print("Python".title())
+
+print("5".isdigit())
+print("1".isdecimal())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
