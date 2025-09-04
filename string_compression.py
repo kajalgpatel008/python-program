84abe98d35595ef5b14d3ac00b534655ba366fd1
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-09-04 23:55:22 +0530

    python program

---
+++ string_compression.py
@@ -0,0 +1,15 @@
+def compress_string(s):
+    if not s:
+        return ""
+    result=""                  ##result=[].......
+    count=1
+    for i in range(1,len(s)):
+        if s[i]==s[i-1]:                ###result.append(s[i]+str(count))
+            count += 1
+        else:
+            result +=(s[i-1]+str(count))
+            count=1
+    result +=(s[-1]+str(count))
+    return result                  ##return "".join(result)
+
+print(compress_string("aabbbcccc"))
