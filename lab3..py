12b627141861a1d37eeb5da473f6b5f067476f7a
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-09-25 13:18:51 +0530

    python practice

---
+++ lab3..py
@@ -0,0 +1,20 @@
+a=int(input("Enter A : "))
+b=int(input("Enter B : "))
+c=a+b
+print("Addition : ",c)
+
+
+
+
+a=float(input("Enter A : "))
+b=float(input("Enter B : "))
+c=a+b
+print("Addition : ",c)
+
+
+
+
+a=input("Enter A : "))
+b=input("Enter B : "))
+c=a+b
+print("Addition : ",c)
