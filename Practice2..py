d4b1b98e41d99e03105c39202fb5f0567ecfb26a
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-09-25 13:19:30 +0530

    python practice

---
+++ Practice2..py
@@ -0,0 +1,16 @@
+l=[12,1.7,-5,-8,"Kajal",True,10,20,5,False]
+
+print(l)
+l.append(400)
+print(l)
+l4=l.copy()
+print(l4)
+l2=[100,300,"maulik"]
+l.extend(l2)
+print(l)
+l3=l.copy()
+print(l3)
+print(l.count(1))
+l3=l
+print(l3)
+
