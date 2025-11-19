32eee80333d10c54c884b58728086c2cee18cb37
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-11-19 18:53:59 +0530

    python practice

---
+++ Sampl.py
@@ -0,0 +1,91 @@
+print("Hello")
+print("welcome to Python Programming Language.",end=" ")
+print("Python is Best.")
+
+a="125463"
+print("A : ",a)
+print(type(a))
+a=False
+print("A : ",a)
+print(type(a))
+
+
+a=30
+b=20
+print("A : ",a)
+print("B : ",b)
+c=a+b
+print("Addition : ",c)
+
+a=30
+b=20
+print("A : ",a)
+print("B : ",b)
+c=a-b
+print("Subtraction : ",c)
+
+a=30
+b=30
+print("A : ",a)
+print("B : ",b)
+c=a*b
+print("Multiplication : ",c)
+
+a=30
+b=20
+print("A : ",a)
+print("B : ",b)
+c=a/b
+print("Division : ",c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
