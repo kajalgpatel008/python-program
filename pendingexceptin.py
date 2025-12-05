b68682cb454c35e995896efbc08fb4f6773b729b
kajalgpatel008 <kajalgpatel008@gmail.com>
2025-12-05 16:09:06 +0530

    python practice

---
+++ pendingexceptin.py
@@ -0,0 +1,19 @@
+print("start code")
+
+try:
+    a=int(input("Enter A : "))
+    b=int(input("Enter B : "))
+    c=a/b
+    print("Division : ",c)
+    l=[1,2,3,4,5]
+    index=int(input("enter index Number To Access List Variable : "))
+    print(l[index])
+    s="Kajal Maulik"
+    print(s.casefold())
+    
+except Exception as e:
+    print("Exception Caught : ",e)
+print("End Code")
+
+
+    
