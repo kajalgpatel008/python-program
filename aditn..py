76290b10bbff66037d6b3bbd3906e516abb5d2a8
kmpatel <kgpatel008@gmail.com>
2026-03-08 00:55:39 +0530

    python practice

---
+++ aditn..py
@@ -0,0 +1,28 @@
+a=30
+b=20
+print("A : ",a)
+print("B : ",b)
+c=a+b
+print("Addition : ",c)
+
+a=30
+b=20
+print("A : ",a)
+print("B : ",b)
+c=a-b
+print("Subtraction : ",c)
+
+a=30
+b=20
+print("A : ",a)
+print("B : ",b)
+c=a*b
+print("Multiplication : ",c)
+
+a=30
+b=20
+print("A : ",a)
+print("B : ",b)
+c=a/b
+print("Division : ",c)
+
